fd38d5c79c7ac23c12fb3d58e0280865331d87cf
Shyam <rsambhetwal@gmail.com>
2023-01-26 02:54:21 +0200

    PlaceList Component added to display the  required lists from the place

--- frontendreact/src/App.js
+++ frontendreact/src/App.js
@@ -1,31 +1,15 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Place from "./components/Place";
+import PlaceList from "./components/PlaceList";
+
 
 const App = () => {
-const [location,setLocation]= useState([]);
-  useEffect(()=>{
-    axios.get("http://localhost:3001/",{
-      params: {
-        limit: 10
-      }
-    })
-    .then((res)=>{
-      setLocation(res.data.data)})
-    .catch(e=>alert('error'))
-  },[])
- 
+
   return (
-    
-    <div className="container">
-      <h1>Helsinki Places</h1>
-      <div>
-        {location.map((place) => (
-          <Place key={place.id} name={place.name.en} isOpen={true} address= {place.location.address.street_address} />
-        ))}
-      </div>
-    </div>
-  );
+       <div className = "App">
+        <PlaceList />
+       </div>
+      );
   
-};
+  };
 export default App;
---
+++ frontendreact/src/components/PlaceList.js
@@ -0,0 +1,33 @@
+import React,{ useState, useEffect  } from "react";
+import axios from "axios";
+import Place from "./Place";
+
+const PlaceList =() =>{
+
+    const [location,setLocation]= useState([]);
+    useEffect(()=>{
+      axios.get("http://localhost:3001/",{
+        params: {
+          limit: 10
+        }
+      })
+      .then((res)=>{
+        setLocation(res.data.data)})
+      .catch(e=>alert('error'))
+    },[])
+   
+    return(
+        <div className="container">
+        <h1>Helsinki Places</h1>
+        <div className="location-cards">
+            <div>
+                {location.map((place) => (
+                <Place key={place.id} name={place.name.en} isOpen={true} address= {place.location.address.street_address} />
+            ))}
+             </div>
+        </div>
+    </div>
+    );
+        };
+
+export default PlaceList
